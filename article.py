ed5d60604c60276598033fdb8fbd1d2a9dbcf397
Elison Crum <30413278+elisoncrum@users.noreply.github.com>
2018-12-29 23:54:55 -0600

    shit ton of stuff

--- article.py
+++ article.py
@@ -1,56 +1,81 @@
 import re
+import os
 from datetime import datetime
 
-def markup(raw):
-	reSearchLink = r'!\(([\s\S]*?)\)\[([\s\S]*?)\]'
-	reLinkReplace = r'<a href="\2">\1</a>'
-	reSearchText = r'{0}([\s\S]*?){0}'
-	textFormat = [r'<i>\1</i>', r'<strong>\1</strong>', r'<strong><i>\1</i><strong>']
+class Blog:
+	staticPath = 'static/'
+	articlePath = 'static/articles/'
 
-	raw = re.sub(reSearchLink, reLinkReplace, raw)
-	for i in range(3, 0, -1):
-		raw = re.sub(reSearchText.format('\*' * i), textFormat[i - 1], raw)
+	def __init__(self):
+		# FontAwesome
+		self.footerIconSize = 'fa-3x'
+		self.footerIcons = [
+			['fab fa-github', 'https://github.com/elisoncrum'], 
+			['fab fa-instagram', 'https://www.instagram.com/elison.hike/'], 
+			['far fa-envelope', 'mailto:#elisoncrum@gmail.com'], 
+			['fas fa-cog', '#']]
 	
-	return raw
-	
-data = {
-	'title': None,
-	'date': None,
-	'content': [
-	]
-}
+	def openArticles(self):
+		files = os.listdir(Blog.articlePath)
+		self.articles = [self.convertArticles(article) for article in files]
 
-article = open('static/articles/1.article', 'r')
+	def sortArticles(self):
+		pass
 
-reTagPattern = '```(\w*)\n([\s\S]*?)```'
-reResult = re.findall(reTagPattern, article.read(), re.MULTILINE)
-reResult = [(tag.strip(), content.replace('\n', ' ')) for tag, content in reResult]
-article.seek(0)
+	def markup(self, raw):
+		reSearchLink = r'!\(([\s\S]*?)\)\[([\s\S]*?)\]'
+		reLinkReplace = r'<a href="\2">\1</a>'
+		reSearchText = r'{0}([\s\S]*?){0}'
+		textFormat = [r'<i>\1</i>', r'<strong>\1</strong>', r'<strong><i>\1</i><strong>']
 
-index = 0
-iterResult = 0
-
-for line in article.readlines():
-	line = line.strip()
-
-	reCommand = re.match('!\(([^\)]+)\)\[([^\)]+)\]', line)
-	reElem = re.match('```(\w+)', line)
-	if index == 0:
-		data['title'] = line
-	elif index == 1:
-		date = datetime.strptime(line, '%m-%d-%y')
-		data['date'] = [line, date.strftime('%b %d, %Y').replace(' 0', ' ')]
-	
-	elif reElem:
-		content = reResult[iterResult][1]
-		content = markup(content)
-		data['content'].append(('elem', reElem.group(1), reResult[iterResult][1]))
-		iterResult += 1
-
-	elif reCommand:
-		if '|' in reCommand.group(0):
-			data['content'].append(('sldshw', reCommand.group(2).split('|'), reCommand.group(1).split('|')))
-		else:
-			data['content'].append(('img', reCommand.group(2), reCommand.group(1)))
+		raw = re.sub(reSearchLink, reLinkReplace, raw)
+		for i in range(3, 0, -1):
+			raw = re.sub(reSearchText.format('\*' * i), textFormat[i - 1], raw)
 		
-	index += 1
+		return raw
+
+	def convertArticles(self, fileName):
+		articleData = {
+			'title': None,
+			'date': None,
+			'content': [ ]
+		}
+
+		article = open(Blog.articlePath + fileName, 'r')
+		reTagPattern = '```(\w*)\n([\s\S]*?)```'
+		reResult = re.findall(reTagPattern, article.read(), re.MULTILINE)
+		reResult = [(tag.strip(), content.replace('\n', ' ')) for tag, content in reResult]
+		article.seek(0)
+
+		index = 0
+		iterResult = 0
+
+		for line in article.readlines():
+			line = line.strip()
+
+			reCommand = re.match('!\(([^\)]+)\)\[([^\)]+)\]', line)
+			reElem = re.match('```(\w+)', line)
+			if index == 0:
+				articleData['title'] = line
+			elif index == 1:
+				date = datetime.strptime(line, '%m-%d-%y')
+				articleData['date'] = [line, date.strftime('%b %d, %Y').replace(' 0', ' ')]
+			
+			elif reElem:
+				content = reResult[iterResult][1]
+				content = self.markup(content)
+				articleData['content'].append(('elem', reElem.group(1), reResult[iterResult][1]))
+				iterResult += 1
+
+			elif reCommand:
+				if '|' in reCommand.group(0):
+					articleData['content'].append(('sldshw', reCommand.group(2).split('|'), reCommand.group(1).split('|')))
+				else:
+					articleData['content'].append(('img', reCommand.group(2), reCommand.group(1)))
+				
+			index += 1
+		
+		return articleData
+
+blog = Blog()
+blog.openArticles()
--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, send_from_directory
+from article import Blog
+
+blog = Blog()
 
 app = Flask('app')
 
+
+@app.route('/static/css<path:path>')
+def send_js(path):
+    return send_from_directory('static/css', path)
+
 @app.route('/')
 def home():
-	return render_template('main.html', article = [('h1','val1'),('h2','val2'),('h3','val3'),('h4','val4')])
+	return render_template('main.html',
+						blog = blog,
+						file_list = os.listdir('static/css')
+					)
 
-app.run(debug=True)
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.png')
+
+app.run(host='0.0.0.0', debug=True)
