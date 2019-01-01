4b74e1c4fc9477b4eaf8404153020b7235d3ff3b
Elison Crum <30413278+elisoncrum@users.noreply.github.com>
2019-01-01 02:06:14 -0600

    app name

---
+++ app.py
@@ -0,0 +1,25 @@
+import os
+from flask import Flask, render_template, send_from_directory
+from article import Blog
+
+blog = Blog()
+blog.openArticles()
+
+app = Flask('app')
+
+@app.route('/static/css<path:path>')
+def send_js(path):
+    return send_from_directory('static/css', path)
+
+@app.route('/')
+def home():
+	return render_template('main.html',
+						blog = blog,
+						css_file_list = os.listdir('static/css')
+					)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.png')
+
+app.run(host='0.0.0.0', port=80)
